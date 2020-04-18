5ea2608c8d3c2888035c577d056d5fcda4e3c922
Xiaonan Shen <s@sxn.dev>
2020-04-18 01:00:18 -0700

    Fix update check

--- update-check.sh
+++ update-check.sh
@@ -2,13 +2,19 @@
 
 set -ex
 
+REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+# Switch to master branch
+git clone "${REMOTE_REPO}" master
+cd master
+
 source ./releaserc
 
 JSON_CONTENT=$(curl -q https://protonmail.com/download/current_version_linux.json)
 URL=$(echo ${JSON_CONTENT} | sed -n "s/^.*\"DebFile\":\"\([a-z0-9:/._-]*\)\".*$/\1/p")
 CURR_RELEASE=$(echo $URL | sed -n "s/https:\/\/protonmail.com\/download\/protonmail-bridge_\([0-9.-]*\)_amd64.deb/\1/p")
 
-if [[ RELEASE != CURR_RELEASE ]]; then
+if [[ $RELEASE != $CURR_RELEASE ]]; then
     echo "New release found: ${CURR_RELEASE}"
 
     # bump up to new release
@@ -22,7 +28,6 @@ if [[ RELEASE != CURR_RELEASE ]]; then
     git tag -a "v${CURR_RELEASE}" -m "Release ${CURR_RELEASE}"
 
     # push
-    REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
     git push "${REMOTE_REPO}" master
     git push "${REMOTE_REPO}" master --tags
 
