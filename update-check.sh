2c9ef4cd8de176295c253c8837c2028702b0cb50
Xiaonan Shen <s@sxn.dev>
2020-05-31 21:45:01 -0700

    Improve update check

--- update-check.sh
+++ update-check.sh
@@ -2,39 +2,33 @@
 
 set -ex
 
-REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+REMOTE_REPO="https://${GITHUB_ACTOR}:${PERSONAL_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
-# Switch to master branch
-git clone "${REMOTE_REPO}" master
-cd master
-
-source ./releaserc
+VERSION=`cat VERSION`
 
 JSON_CONTENT=$(curl -q https://protonmail.com/download/current_version_linux.json)
 URL=$(echo ${JSON_CONTENT} | sed -n "s/^.*\"DebFile\":\"\([a-z0-9:/._-]*\)\".*$/\1/p")
-CURR_RELEASE=$(echo $URL | sed -n "s/https:\/\/protonmail.com\/download\/protonmail-bridge_\([0-9.-]*\)_amd64.deb/\1/p")
+CURR_VERSION=$(echo $URL | sed -n "s/https:\/\/protonmail.com\/download\/protonmail-bridge_\([0-9.-]*\)_amd64.deb/\1/p")
 
-if [[ $RELEASE != $CURR_RELEASE ]]; then
-    echo "New release found: ${CURR_RELEASE}"
+if [[ $VERSION != $CURR_VERSION ]]; then
+    echo "New release found: ${CURR_VERSION}"
 
     # bump up to new release
-    sed -i "s/^RELEASE=.*$/RELEASE=${CURR_RELEASE}/" releaserc
+    cat ${CURR_VERSION} > VERSION
 
     # commit
     git config --local user.email "actions@github.com"
     git config --local user.name "Github Action"
-    git add releaserc
-    git commit -m "Release ${CURR_RELEASE}" --author="Xiaonan Shen <s@sxn.dev>"
-    git tag -a "v${CURR_RELEASE}" -m "Release ${CURR_RELEASE}"
+    git add VERSION
+    git commit -m "Bump version to ${CURR_VERSION}" --author="Xiaonan Shen <s@sxn.dev>"
 
     # push
     git push "${REMOTE_REPO}" master
-    git push "${REMOTE_REPO}" master --tags
 
-    # trigger actions
-    curl -H "Accept: application/vnd.github.everest-preview+json" \
-        -H "Authorization: token ${PERSONAL_TOKEN}" \
-        --request POST \
-        --data '{"event_type": "build"}' \
-        https://api.github.com/repos/${GITHUB_REPOSITORY}/dispatches
+    # # trigger actions
+    # curl -H "Accept: application/vnd.github.everest-preview+json" \
+    #     -H "Authorization: token ${PERSONAL_TOKEN}" \
+    #     --request POST \
+    #     --data '{"event_type": "build"}' \
+    #     https://api.github.com/repos/${GITHUB_REPOSITORY}/dispatches
 fi
