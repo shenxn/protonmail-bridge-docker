961f9c70dd89976d482db342ec891c27e96c09e8
Xiaonan Shen <s@sxn.dev>
2020-05-31 21:36:46 -0700

    Fix master tag

--- set-env.sh
+++ set-env.sh
@@ -4,7 +4,7 @@ set -ex
 
 VERSION=`cat VERSION`
 
-if [[ $GITHUB_REF == "master" ]]; then
+if [[ $GITHUB_REF == "refs/heads/master" ]]; then
     echo "::set-env name=TAGS::latest,${VERSION}"
 else
     echo "::set-env name=TAGS::dev,${VERSION}-dev"
