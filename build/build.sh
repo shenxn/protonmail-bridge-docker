72c40c7a05fd3f52fc9a4f648b88b15e64c41f20
Xiaonan Shen <s@sxn.dev>
2020-06-01 07:51:40 -0700

    Fix version tag

--- build/build.sh
+++ build/build.sh
@@ -7,7 +7,7 @@ VERSION=`cat VERSION`
 # Clone new code
 git clone https://github.com/ProtonMail/proton-bridge.git
 cd proton-bridge
-git checkout $VERSION
+git checkout v$VERSION
 
 # Enable debug log
 sed -i "s/build desktop/-debug build desktop/" Makefile
