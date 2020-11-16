841f177a79607909b546f3fe761860dd07827e79
Xiaonan Shen <s@sxn.dev>
2020-11-15 21:14:48 -0800

    Fix update checker

--- update-check.sh
+++ update-check.sh
@@ -6,7 +6,12 @@ VERSION=`cat VERSION`
 
 JSON_CONTENT=$(curl -q https://protonmail.com/download/current_version_linux.json)
 URL=$(echo ${JSON_CONTENT} | sed -n "s/^.*\"DebFile\":\"\([a-z0-9:/._-]*\)\".*$/\1/p")
-CURR_VERSION=$(echo $URL | sed -n "s/https:\/\/protonmail.com\/download\/protonmail-bridge_\([0-9.-]*\)_amd64.deb/\1/p")
+CURR_VERSION=$(echo $URL | sed -n "s/https:\/\/protonmail.com\/.*_\([0-9.-]*\)_.*.deb/\1/p")
+
+if [[ -z $CURR_VERSION ]]; then
+    echo "Failed to get new version. Existing."
+    exit 1
+fi
 
 if [[ $VERSION != $CURR_VERSION ]]; then
     echo "New release found: ${CURR_VERSION}"
