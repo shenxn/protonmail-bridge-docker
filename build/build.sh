252397a86db69aff53c64c5fb50819b272e777d4
Xiaonan Shen <s@sxn.dev>
2022-11-06 13:15:08 +0800

    Switch to carlosedp/golang for riscv64 support

--- build/build.sh
+++ build/build.sh
@@ -9,14 +9,15 @@ git clone https://github.com/ProtonMail/proton-bridge.git
 cd proton-bridge
 git checkout v$VERSION
 
-# Build
-if ! make build-nogui ; then
-	# If build fails it's probably because it is a 32bit
-	# system and there was a overflow error on the parser
+ARCH=$(uname -m)
+if [[ $ARCH == "armv7l" ]] ; then
+	# This is expected to fail, and we use the following patch to fix
+	make build-nogui || true
+	# For 32bit architectures, there was a overflow error on the parser
 	# This is a workaround for this problem found at:
 	#   https://github.com/antlr/antlr4/issues/2433#issuecomment-774514106
 	find $(go env GOPATH)/pkg/mod/github.com/\!proton\!mail/go-rfc5322*/ -type f -exec sed -i.bak 's/(1<</(int64(1)<</g' {} +
-
-	# Try again after implementing the workaround
-	make build-nogui
 fi
+
+# Build
+make build-nogui
