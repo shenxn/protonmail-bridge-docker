b62e01689c79bc552747fb782be3140a0c6b4143
Simon Ungar Felding <sife@adm.ku.dk>
2024-12-06 14:43:17 +0100

    add friendly error messages for when the container hasn't been initialized properly.

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -22,6 +22,13 @@ else
     # delete lock files if they exist - this can happen if the container is restarted forcefully
     find $HOME -name "*.lock" -delete
 
+    # give friendly error if you don't have protonmail data
+    find $HOME | grep -q . || echo "No files found - start the container with the init command, or copy/mount files into it at $HOME first. Sleeping 5 minutes before exiting so you have time to copy the files over." && sleep 300 && exit 1
+    # give friendly error if the user doesn't own the data
+    if [[ $(id -u) != 0 ]]; then
+        find $HOME -not -user $(id -u) | grep -q . || echo "You do not own the data in $HOME. Please chown it to $(id -u), run the container as the owner of the data or run the container as root." && exit 1
+    fi
+
     # socat will make the conn appear to come from 127.0.0.1
     # ProtonMail Bridge currently expects that.
     # It also allows us to bind to the real ports :)
