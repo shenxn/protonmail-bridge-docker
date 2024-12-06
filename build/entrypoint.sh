ac55256b612a53b02afdeff5340266b6a6df0324
Simon Ungar Felding <sife@adm.ku.dk>
2024-12-06 13:45:31 +0100

    fix restarts

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -19,6 +19,8 @@ if [[ $1 == init ]]; then
     /protonmail/proton-bridge --cli $@
 
 else
+    # delete lock files if they exist - this can happen if the container is restarted forcefully
+    find $HOME -name "*.lock" -delete
 
     # socat will make the conn appear to come from 127.0.0.1
     # ProtonMail Bridge currently expects that.
@@ -28,13 +30,10 @@ else
         socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
     fi
 
-    socat TCP-LISTEN:1025,fork TCP:127.0.0.1:1025 &
-    socat TCP-LISTEN:1143,fork TCP:127.0.0.1:1143 &
+    socat TCP-LISTEN:2025,fork TCP:127.0.0.1:1025 &
+    socat TCP-LISTEN:2143,fork TCP:127.0.0.1:1143 &
 
     # Start protonmail
-    # Fake a terminal, so it does not quit because of EOF...
-    rm -f faketty
-    mkfifo faketty
-    cat faketty | /protonmail/proton-bridge --cli $@
+    /protonmail/proton-bridge --noninteractive $@
 
 fi
