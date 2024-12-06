db779f4e521b276211935cdeb4f623bb2f27ab8b
Simon Ungar Felding <sife@adm.ku.dk>
2024-12-06 12:27:01 +0100

    add (backwards compatible) support for running rootless

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -23,8 +23,13 @@ else
     # socat will make the conn appear to come from 127.0.0.1
     # ProtonMail Bridge currently expects that.
     # It also allows us to bind to the real ports :)
-    socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
-    socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
+    if [[ $(id -u) == 0 ]]; then
+        socat TCP-LISTEN:25,fork TCP:127.0.0.1:1025 &
+        socat TCP-LISTEN:143,fork TCP:127.0.0.1:1143 &
+    fi
+
+    socat TCP-LISTEN:1025,fork TCP:127.0.0.1:1025 &
+    socat TCP-LISTEN:1143,fork TCP:127.0.0.1:1143 &
 
     # Start protonmail
     # Fake a terminal, so it does not quit because of EOF...
